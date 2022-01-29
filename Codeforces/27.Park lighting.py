1c6ef1e2b468955e4595d00da33fe61f55aaee90
Thushani <9thushani7@gmail.com>
2022-01-29 10:20:28 +0530

    2022/01/29

--- Codeforces/27.Park lighting.py
+++ Codeforces/27.Park lighting.py
@@ -1,9 +1,11 @@
 ans=[]
 for i in range(int(input())):
-    n,m=list(map(int,input().split()))
-    if (m+1)%2==0:
-        ans.append(str((m//2)*n+(n+1)//2))
-    elif ((m+1)%2==1):
-        ans.append(str((m//2)*n))
+    a,b=map(int,input().split())
+    print((a*b+1)//2)
+    # n,m=list(map(int,input().split()))
+    # if (m+1)%2==0:
+        # ans.append(str((m//2)*n+(n+1)//2))
+    # elif ((m+1)%2==1):
+        # ans.append(str((m//2)*n))
 
-print("\n".join(ans))
+# print("\n".join(ans))
---
+++ Codeforces/28.Resorting Three Numbers.py
@@ -0,0 +1,3 @@
+p,q,r,s=sorted(list(map(int,input().split())))
+print(s-p,s-q,s-r)
+    
