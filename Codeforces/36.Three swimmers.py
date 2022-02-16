590cc73ab20b9d5398adfc365114418bcbd849a6
Thushani <9thushani7@gmail.com>
2022-02-16 23:11:51 +0530

    2022/02/16

--- Codeforces/36.Three swimmers.py
+++ Codeforces/36.Three swimmers.py
@@ -1,22 +1,3 @@
 for i in range(int(input())):
-    lst=list(map(int,input().split()))
-    p=lst.pop(0)
-    if p in lst:
-        print (0)
-    elif p> max(lst):
-        j=2
-        while True:
-            lst=[i*j for i in lst]
-            j+=1
-            ans=[]
-            for i in lst:
-                if p<i:
-                    ans=[i-p for i in lst]
-                    print (abs(min(ans)))
-                    break
-                else:
-                    pass
-            break
-               
-    else:
-        print (min(lst)-p)
+    p, a, b, c = list(map(int,input().split()))
+    print(min(-p % a, -p % b, -p % c))
