09042bd11fdb7853e2ed17d453a4c438cf003468
Thushani <9thushani7@gmail.com>
2022-02-19 11:26:36 +0530

    19/02/2022

--- Codeforces/37.Reorder.py
+++ Codeforces/37.Reorder.py
@@ -1,13 +1,15 @@
 for i in range(int(input())):
     n,m= list(map(int,input().split()))
-    m=float(m)
-    lst=sorted(list(map(int,input().split())))
-    sum=0
-    for i in range(1,n+1):
-        for j in range(i,n+1):
-            sum+= lst[j-1]/j
+    # m=float(m)
+    lst=sum(list(map(int,input().split())))
+    # print(lst)
+    # sum=0
+    # for i in range(1,n+1):
+        # for j in range(i,n+1):
+            # print(lst[j-1],j)
+            # sum+= lst[j-1]/j
     
-    if sum==m:
+    if lst==m:
         print("YES")
     else:
         print("NO")
