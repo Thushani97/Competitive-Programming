b4d1728d30f0cfa44f198bb1e5be1a247c0fdf92
Thushani <9thushani7@gmail.com>
2022-02-27 22:49:25 +0530

    27/02/2022

--- Codeforces/41.Who is opposite.py
+++ Codeforces/41.Who is opposite.py
@@ -2,8 +2,11 @@ for i in range(int(input())):
     a,b,c= list(map(int,input().split()))
     r= abs(a-b)
     circle=r*2
-    if r>1 and c+r<= circle:
-        print (c+r)
+    if r>1 and c<= circle and a<= circle and b<=circle:
+        if c+r<=circle:
+            print (c+r)
+        else:
+            print(c-r)
     else:
         print(-1)
 
