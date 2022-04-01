6e745cea7093d0a55c603f076484952520e88609
Thushani <9thushani7@gmail.com>
2022-04-02 01:40:11 +0530

    02/04/2022

--- Hackerrank/Sale by match.py
+++ Hackerrank/Sale by match.py
@@ -1,8 +1,29 @@
-n=(int(input()))
-ar = list(map(int,input().split()))
-dic={}
+import math
+import os
+import random
+import re
+import sys
 
-for i in ar:
-    dic[i]= (ar.count(i))//2
+
+def sockMerchant(n, ar):
+    # Write your code here
    
-return(sum(dic.values()))
+    dic={}
+
+    for i in ar:
+        dic[i]= (ar.count(i))//2
+    
+    return(sum(dic.values()))
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
