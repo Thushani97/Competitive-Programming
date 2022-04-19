549a11d7b9dba635fc6653543990091840e31ebd
Thushani <9thushani7@gmail.com>
2022-04-20 02:35:54 +0530

    20/04/2022

--- Codeforces/47.Arithmetic Array.py
+++ Codeforces/47.Arithmetic Array.py
@@ -1,8 +1,10 @@
 for i in range(int(input())):
     k=int(input())
+    sum_=0
     lst= list(map(int,input().split()))
     if sum(lst) >=len(lst):
-        print("ans",sum(lst)-len(lst))
-    else:
-        
-    #print(abs(k-(sum(lst))))
+        print(sum(lst)-len(lst))
+    elif (sum(lst)<0 or sum(lst)<k):
+       
+        print (1)
+ 
